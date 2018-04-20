d35cbf8136a01a1ca3d85c4f92d02bdecff592ec
StarSeeker45 <seekerofstar45@gmail.com>
2018-04-20 11:07:58 +0800

    small joke update

--- src/app/jokelist.component.ts
+++ src/app/jokelist.component.ts
@@ -12,7 +12,7 @@ export class JokeListComponent{
     jokes: Joke[];
     constructor(){
         this.jokes = [
-            new Joke('What did the cheese say when it looked in the mirror?', 'Halloumi (Hello Me)'),
+            new Joke('What did the cheese say when it looked in the mirror?', 'Halloumi (Hello You)'),
             new Joke('What kind of cheese do you use to disguise a small horse?','Mask-a-pony (Mascarpone)'),
             new Joke('A kid threw a lump of cheddar at me','It was not very mature'),
         ];
